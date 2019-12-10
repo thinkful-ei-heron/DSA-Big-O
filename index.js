c44fc3bf36b28275e827f63bd5c37b580506d603
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-12-10 14:00:33 -0800

    finished

--- index.js
+++ index.js
@@ -71,6 +71,8 @@
 // ---- Solving Iteratively  
 // ---- O(n) Linear Complexity
 
+// Solving Recursively complexity is O(n);
+
 // const countingSheep = function (num){
 //     for(let i = 0; i <= num; i++){
 //         if(i === num){
@@ -87,6 +89,7 @@
 // Power Calculator
 // 2. Write a function called `powerCalculator()` that takes two parameters, an integer as a base, and another integer as an exponent.The function returns the value of the base raised to the power of the exponent.Use only exponents greater than or equal to 0(positive numbers)
 //Linear ~ O(n)
+// Solving Recursively complexity is O(n);
 
 // const powerCalculator = (base, number) => {
 //   let result = 1;
@@ -100,6 +103,7 @@
 // console.log(powerCalculator(5, 12));
 
 // Drill 12.3 Reverse String
+// Solving Recursively complexity is O(n);
 // -- O(n) Linear time complexity as it's iterating over every number before the input. 
 // const reverseString = (str) => {
 //     let currentString = str;
@@ -118,10 +122,143 @@
 // The nth triangular number is the number of dots composing a triangle with n dots on a side,
 //   and is equal to the sum of the n natural numbers from 1 to n.
 // This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45
-// Constant O(c) 
-const triangleNumber = (num) => {
-  return num * (num + 1) / 2;
-};
+// Constant O(1) 
+// Solving Recursively complexity is O(n);
+// const triangleNumber = (num) => {
+//   return num * (num + 1) / 2;
+// };
 
-console.log(triangleNumber(2));
+//console.log(triangleNumber(2));
+
+
+// Tower of Hanoi
+
+// function TOH(num, source = [], dest = [], temp = [], prevMove = null){
+//     const poleSource = source;
+//     const poleDest = dest;
+//     const poleTemp = temp;
+
+//     if(num === 1){
+//         print(source, dest);
+//     }
+
+//     // not previously moved disk
+//     // try moving largest disk
+//     // try moving second largest etc...
+//     // if poleDest[0] === disksize
+ 
+//         if (getLargest(poleSource) < getSmallest(poleDest)) {
+//             poleDest.push(poleSource.pop());
+//             print(poleSource, poleDest);
+//             return TOH(num, poleSource, poleDest, poleTemp);
+//         } else if (getSmallest(poleSource)){
+// return;
+//         }
+        
+// return;
+//     }
+    
+
+// function print(source, dest){
+//     console.log(`Solving: ${source} -> ${dest}`);
+// }
+// function getSmallest(arr){
+//     return Math.min.apply(Math, arr); 
+// }
+
+// function getLargest(arr) {
+//     return Math.max.apply(Math, arr);
+// }
+// TOH(6, [6,5,4,3,2,1], [], []);
+
+//const array = [1,3];
+//console.log(getLargest(array));
+
+
+// function TOH02(num, source, dest, temp){
+//     if(num === 0) {
+//         return;
+//     }
+//     if(num === 1){
+//         print02(source, dest);
+//         return;
+//     }
+    
+//     TOH02( num -1, source, temp, dest);
+//     print02(source, dest);
+
+//     TOH02( num -1, temp, dest, source);
+// }
+
+// function print02(source, dest){
+//     console.log(`solving ${source} to ${dest}`);
+
+// }
+
+
+//TOH02(3, 'src','dst', 'tmp');
+
+
+
+
+
+// Drill 12.string splitter
+//let input= '1/21/2018';
+//Output: 1212018 OR["1", "21", "2018"]
+// Linear O(n)
+// Solving Recursively complexity is O(n);
+
+// const stringSplitter = (str, sep) => {
+//     let newString = '';
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === sep) {
+//             continue;
+//         }
+//         newString += str[i];
+//     }
+//     return newString;
+// };
+
+//console.log(stringSplitter(input, '/'));
+
+
+// Drill 12.factorial
+// Linear O(n) complexity.
+// Solving Recursively complexity is O(n);
+
+// function factorial(num) {
+//     let result = num;
+//     for(let i = num - 1; i > 0; i--){
+//         result *= i ;
+//     }
+//     return result;
+// }
+// console.log(factorial(5));
+
+// O(n)
+// Solving Recursively complexity is O(n^2)
+// function fibonacci(n) {
+//     let result = [];
+//     for (let i = 0; i < n; i++){
+//         if (i < 2) {
+//             result.push(1);
+//         } else {
+//             result.push(result[i-1] + result[i-2]);
+//         }
+//     }
+//     return result;
+// }
+// console.log(fibonacci(7));
+
+
+// Anagrams recursive complexity
+// Recursive complexity is O(x^n)
+
+
+// Org Chart 
+// // Solving Recursively complexity is O(n);
+
+
+// Binary 
+// Solving Recursively complexity is O(log(n);
 
